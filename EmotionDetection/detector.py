76a9b0393bcaedf88256709a2b577f3479966aa1
svellika <srinivas1978@gmail.com>
2025-07-14 13:06:33 -0400

    Initial Commit

---
+++ EmotionDetection/detector.py
@@ -0,0 +1,31 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyze):
+
+    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input_json = { "raw_document": { "text": text_to_analyze } }
+
+    response = requests.post(url, headers=headers, json=input_json)
+    response_text = response.text
+    data = json.loads(response_text)
+    emotions = data['emotionPredictions'][0]['emotion']
+
+    highest_score = 0
+    dominant_emotion = ""
+
+    # Loop through each emotion and its score
+    for emotion, score in emotions.items():
+        if score > highest_score:
+            highest_score = score
+            dominant_emotion = emotion
+    
+    emotions['dominant_emotion'] = dominant_emotion
+
+    return emotions    
+
+if __name__ == "__main__":
+    text = "I am so happy I'm doing this."
+    output = emotion_detector(text)
+    print(output)
